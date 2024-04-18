6f58fc5822062079c21b189d71e6663684bd74b1
EricFilipe <ericfilipe125@gmail.com>
2024-04-18 10:34:11 -0300

    Trying connection

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express()
 const cors = require('cors');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://api-youtube-project.onrender.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods", 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
@@ -21,5 +21,6 @@ app.use(express.json())
 app.use('/user', useRoutes)
 app.use('/videos', videosRoutes)
 
+const port = process.env.PORT || 4000; 
 
-app.listen(4000)
+app.listen(port)
