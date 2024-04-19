0ab92c2873fcf7235fcf9d9cebcaeb37525569f4
EricFilipe <ericfilipe125@gmail.com>
2024-04-19 10:01:51 -0300

    Fix bugs

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,9 @@ app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://api-youtube-project.onrender.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
 });
