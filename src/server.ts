b9d8ae21f611532ae75b60590ee6cd608fa1ab12
EricFilipe <ericfilipe125@gmail.com>
2024-04-19 10:14:17 -0300

    testing localhost

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.header("Access-Control-Allow-Methods", 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
